d6290f1409eee9a06e747ecf208f8859a5c550b9
Yaroslav Khodyrev <yrhod47@gmail.com>
2024-10-29 23:04:39 +0300

    proto interfaces for auth n user

--- internal/auth/service/auth_service.go
+++ internal/auth/service/auth_service.go
@@ -1,11 +1,1 @@
 package auth
-
-import (
-	"context"
-	"github.com/grpc-vtb/internal/auth/proto"
-)
-
-type AuthService interface {
-	Login(ctx context.Context, req *proto.UserAuth) (*proto.TokenResponse, error)
-	ValidateToken(ctx context.Context, req *proto.TokenRequest) (*proto.RoleResponse, error)
-}
