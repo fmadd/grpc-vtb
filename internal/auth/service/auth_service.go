6b963e8153ee46eb77e4feac2b107e7c80e087b0
Yaroslav Khodyrev <yrhod47@gmail.com>
2024-10-29 22:43:12 +0300

    generation auth proto

--- internal/auth/service/auth_service.go
+++ internal/auth/service/auth_service.go
@@ -2,25 +2,10 @@ package auth
 
 import (
 	"context"
+	"github.com/grpc-vtb/internal/auth/proto"
 )
 
-// UserAuth - структура для представления аутентификационных данных пользователя.
-type UserAuth struct {
-	Username string
-	Password string
-}
-
-// TokenResponse - структура для представления ответа с токеном.
-type TokenResponse struct {
-	AccessToken string
-	ExpiresIn   int64
-}
-
-// AuthService - интерфейс для взаимодействия с модулем аутентификации.
 type AuthService interface {
-	// Login - метод для получения токена с использованием данных пользователя.
-	Login(ctx context.Context, auth UserAuth) (TokenResponse, error)
-
-	// ValidateToken - метод для проверки валидности JWT токена.
-	ValidateToken(ctx context.Context, token string) (string, error)
+	Login(ctx context.Context, req *proto.UserAuth) (*proto.TokenResponse, error)
+	ValidateToken(ctx context.Context, req *proto.TokenRequest) (*proto.RoleResponse, error)
 }
