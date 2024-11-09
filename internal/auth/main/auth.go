37fdae3460903f584857d94bfbc1b7458f4dcaa9
Yaroslav Khodyrev <yrhod47@gmail.com>
2024-11-09 22:23:58 +0300

    update dockerfiles

--- internal/auth/main/auth.go
+++ internal/auth/main/auth.go
@@ -17,26 +17,21 @@ const (
 )
 
 func main() {
-	// Настройка прослушивания порта
 	lis, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		logger.Logger.Fatal("Couldn't start listening", zap.Error(err))
 	}
 
-	// Загрузка серверных TLS-учетных данных
 	serverCreds, err := cert.LoadServerTLSCredentials(serverCertFile, serverKeyFile)
 	if err != nil {
 		logger.Logger.Fatal("Failed to load TLS server credits", zap.Error(err))
 	}
 
-	// Инициализация клиента Keycloak для AuthHandler
 	client := gocloak.NewClient("http://localhost:8080")
 	authService := handler.NewAuthHandler(client, "realm", "clientid", "clientsecret")
 
-	// Создание gRPC-сервера с использованием серверных учетных данных для TLS
 	grpcServer := grpc.NewServer(grpc.Creds(serverCreds))
 
-	// Регистрация AuthService на gRPC-сервере
 	proto.RegisterAuthServiceServer(grpcServer, authService)
 
 	logger.Logger.Info("Running an auth server with TLS on port :8081...")
--- internal/user/main/user.go
+++ internal/user/main/user.go
@@ -19,13 +19,11 @@ const (
 )
 
 func main() {
-	// Подключение к auth-серверу с клиентскими сертификатами
 	clientCreds, err := cert.LoadClientTLSCredentials(clientCertFile, clientKeyFile)
 	if err != nil {
 		logger.Logger.Fatal("Client TLS cards could not be loaded to connect to the auto-server", zap.Error(err))
 	}
 
-	// Указываем префикс dns для URI
 	authConn, err := grpc.NewClient("dns:///localhost:8081", grpc.WithTransportCredentials(clientCreds))
 	if err != nil {
 		logger.Logger.Fatal("Failed to connect to the auth server", zap.Error(err))
@@ -34,7 +32,6 @@ func main() {
 
 	authClient := proto.NewAuthServiceClient(authConn)
 
-	// Создаем gRPC-сервер для UserHandler с серверными сертификатами
 	serverCreds, err := cert.LoadServerTLSCredentials(serverCertFile, serverKeyFile)
 	if err != nil {
 		logger.Logger.Fatal("Failed to load TLS server credits for the user server", zap.Error(err))
@@ -46,11 +43,9 @@ func main() {
 		AuthClient: authClient,
 	}
 
-	// Регистрируем UserService в gRPC-сервере
 	userProto.RegisterUserServiceServer(srv, userHandler)
 
-	// Запуск gRPC-сервера с прослушиванием защищенного соединения
-	lis, err := net.Listen("tcp", ":50053") // Порт для UserService
+	lis, err := net.Listen("tcp", ":50053")
 	if err != nil {
 		logger.Logger.Fatal("Couldn't start listening", zap.Error(err))
 	}
